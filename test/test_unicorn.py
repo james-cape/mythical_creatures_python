6bb165d0732f0ed25745d9480bc9eb5d6b291cb2
James Cape <james.cape@gmail.com>
2019-09-17 16:18:45 -0600

    Tests unicorn has attributes

---
+++ test/test_unicorn.py
@@ -0,0 +1,34 @@
+import pytest
+from lib.unicorn import *
+# gem 'minitest', '~> 5.2'
+# require 'minitest/autorun'
+# require 'minitest/pride'
+# require_relative '../lib/unicorn'
+
+# class UnicornTest < Minitest::Test
+
+def test_it_has_a_name():
+    unicorn = Unicorn("Robert")
+    assert 0
+    assert unicorn.name == "Robert"
+
+#   def test_it_is_white_by_default
+#     skip
+#     unicorn = Unicorn.new("Margaret")
+#     assert_equal "white", unicorn.color
+#     assert_equal true, unicorn.white?
+#     assert unicorn.white?
+#
+#   def test_it_does_not_have_to_be_white
+#     skip
+#     unicorn = Unicorn.new("Barbara", "purple")
+#     assert_equal "purple", unicorn.color
+#     assert_equal false, unicorn.white?
+#     refute unicorn.white?
+#
+#   def test_unicorn_says_sparkly_stuff
+#     skip
+#     unicorn = Unicorn.new("Johnny")
+#     assert_equal "**;* Wonderful! **;*", unicorn.say("Wonderful!")
+#     assert_equal "**;* I don't like you very much. **;*", unicorn.say("I don't like you very much.")
+#
