7a69870a3d6ffd1ccee883bc88430a7b5080d3d1
jamescape <james.cape@gmail.com>
2019-09-30 08:14:22 -0600

    Adds name and statues array. Adds stare method.

--- lib/medusa.py
+++ lib/medusa.py
@@ -1,3 +1,11 @@
 class Medusa:
+    def __init__(self, name, statues = []):
+        self.name = name
+        self.statues = statues
+
+    def stare(self, victim):
+        self.statues.append(victim)
+
+class Person:
     def __init__(self, name):
         self.name = name
